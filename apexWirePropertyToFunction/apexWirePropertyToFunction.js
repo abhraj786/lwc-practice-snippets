96ffb3f890b0b9f48b0f84ae2e48475eade302a5
Abhishek Raj <abhraj786@gmail.com>
2020-05-03 20:26:14 +0530

    'CleanUpActivity'

--- apexWirePropertyToFunction/apexWirePropertyToFunction.js
+++
@@ -1,7 +0,0 @@
-import { LightningElement, wire } from 'lwc';
-import getContactList from '@salesforce/apex/ContactController.getContactList';
-
-export default class ApexWirePropertyToFunction extends LightningElement {
-
-    @wire(getContactList)contacts;
-}
--- hello2/hello2.js
+++
@@ -1,3 +0,0 @@
-import { LightningElement } from 'lwc';
-
-export default class Hello2 extends LightningElement {}
